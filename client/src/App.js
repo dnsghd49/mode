e8ddf2d6616d3a280b35d2c89912e4d051b21272
Aaron Lu <aaronnoh0409@gmail.com>
2022-05-23 19:07:11 -0400

    adding routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
-import Main from './pages/main';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Main from './pages/Main';
 import Topbar from './components/Topbar';
 import MainNav from './components/MainNav';
 
 function App() {
   return (
     <div className="App">
-      <Topbar />
-      <MainNav />
-      <Main />
+      <BrowserRouter>
+        <Topbar />
+        <MainNav />
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
