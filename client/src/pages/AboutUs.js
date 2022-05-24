f6cac90776550339a6ce44071da5f4ca53d19460
Aaron Lu <aaronnoh0409@gmail.com>
2022-05-23 21:32:21 -0400

    setting up about us page to put some random contents

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -1,8 +1,15 @@
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Container";
+import Col from "react-bootstrap/esm/Container";
+
 function AboutUs() {
     return (
-        <div>
-            this is AboutUs
-        </div>
+        <Container fluid="md">
+            <Row>
+                <Col>1 of 2</Col>
+                <Col>2 of 2</Col>
+            </Row>
+        </Container>
     );
 }
 
