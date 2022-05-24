66fc99d92094f6384b11911ed2e63d00c91456d5
Aaron Lu <aaronnoh0409@gmail.com>
2022-05-23 20:36:05 -0400

    removing some code

--- client/src/components/SlideShow/index.js
+++ client/src/components/SlideShow/index.js
@@ -16,10 +16,6 @@ function SlideShow() {
                             src={one}
                             alt="First slide"
                         />
-                        <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
@@ -27,11 +23,6 @@ function SlideShow() {
                             src={two}
                             alt="Second slide"
                         />
-
-                        <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
@@ -39,11 +30,6 @@ function SlideShow() {
                             src={three}
                             alt="Third slide"
                         />
-
-                        <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                        </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
             </div>
