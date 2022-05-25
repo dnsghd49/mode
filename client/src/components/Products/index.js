522ea46b51d792f6b9a73b13b522c4fa03c3dbb8
Aaron Lu <aaronnoh0409@gmail.com>
2022-05-25 02:50:38 -0400

    fetching data and displaying to the product component

--- client/src/components/Products/index.js
+++ client/src/components/Products/index.js
@@ -3,30 +3,35 @@ import Card from "react-bootstrap/Card"
 import Button from "react-bootstrap/esm/Button";
 
 function Products() {
-    const [githubData, setgithubData] = useState('')
+    const [getData, setGetData] = useState([])
 
     useEffect(() => {
-        fetch('https://api.github.com/users/dnsghd49')
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                setgithubData(data)
-            })
+        const fetchData = async () => {
+            const response = await fetch(`http://localhost:5000/products`)
+            const resData = await response.json()
+            setGetData(resData)
+        }
+        fetchData()
     }, [])
 
-    return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-        </Card>
-    );
+    console.log(getData)
+    return getData.map((product) => {
+        console.log(product)
+        return (
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>{product.product_name}</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+        )
+    });
+
 }
 
 export default Products;
