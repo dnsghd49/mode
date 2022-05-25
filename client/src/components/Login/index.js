deac7dd64a82a36c00d2800e24031f0bea8b8b9a
Aaron Lu <aaronnoh0409@gmail.com>
2022-05-25 00:42:50 -0400

    user signup form takes in the data to the db, tested with postman also tested with client side

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -2,36 +2,12 @@ import "./style.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { useNavigate } from "react-router-dom";
-import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons/faXmark";
 
-async function loginUser(credentials) {
-    return fetch('http://localhost:5000/auth', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(credentials)
-    })
-        .then(data => data.json())
-}
-
 function Login(props, { setToken }) {
-    const [username, setUserName] = useState();
-    const [password, setPassword] = useState();
-
-    const handleSubmit = async e => {
-        e.preventDefault();
-        const token = await loginUser({
-            username,
-            password
-        });
-        console.log(token)
-        setToken(token);
-    }
 
     let navigate = useNavigate();
     const loginRedirect = () => {
@@ -47,14 +23,14 @@ function Login(props, { setToken }) {
                     onClick={() => props.setTrigger(false)}
                     icon={faXmark}
                 />
-                <Form className="login-form" onSubmit={handleSubmit}>
+                <Form className="login-form" >
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label className="login-label">Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" onChange={e => setUserName(e.target.value)} />
+                        <Form.Control type="email" placeholder="Enter email" />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label className="login-label">Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+                        <Form.Control type="password" placeholder="Password"  />
                     </Form.Group>
                     <Button variant="primary" type="submit" onClick={(e) => {loginRedirect(); props.setTrigger(false)}}>
                         Submit
--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -4,27 +4,82 @@ import Button from 'react-bootstrap/Button'
 // import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark } from '@fortawesome/free-solid-svg-icons/faXmark'
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
 
 
 // name password email
 function Signup(props) {
+
+    const [signup, setSignup] = useState({
+        user_name: "",
+        email: "",
+        password: "",
+    });
+
+    const navigate = useNavigate();
+
+    function updateUser(value) {
+        return setSignup((prev) => {
+            return { ...prev, ...value };
+        });
+    }
+
+    // This function will handle the submission.
+    async function onSubmit(e) {
+        e.preventDefault();
+
+        // When a post request is sent to the create url, we'll add a new record to the database.
+        const newPerson = { ...signup };
+
+        await fetch("http://localhost:5000/users/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newPerson),
+        })
+            .catch(error => {
+                window.alert(error);
+                return;
+            });
+
+        setSignup({ user_name: "", email: "", password: "" });
+        navigate("/dashboard");
+    }
+
     return (props.trigger) ? (
         <div className='signup-popup'>
             <div className='signup-popup-item'>
                 {props.children}
                 <FontAwesomeIcon className='fa-lg signup-close-btn' onClick={() => props.setTrigger(false)} icon={faXmark} />
-                <Form onSubmit={() => props.setTrigger(true)} action="/users/signup" method="POST">
+                <Form onSubmit={onSubmit}>
                     <Form.Group className="mb-3 signup-label" controlId="formBasicEmail">
                         <Form.Label>Username</Form.Label>
-                        <Form.Control type="username" placeholder="Enter username" />
+                        <Form.Control
+                            type="username"
+                            placeholder="Enter username"
+                            id="user_name"
+                            value={signup.user_name}
+                            onChange={(e) => updateUser({ user_name: e.target.value })} />
                     </Form.Group>
                     <Form.Group className="mb-3 signup-label" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control
+                            type="email"
+                            placeholder="Enter email"
+                            id="email"
+                            value={signup.email}
+                            onChange={(e) => updateUser({ email: e.target.value })} />
                     </Form.Group>
                     <Form.Group className="mb-3 signup-label" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control
+                            type="password"
+                            placeholder="Password"
+                            id="password"
+                            value={signup.password}
+                            onChange={(e) => updateUser({ password: e.target.value })} />
                     </Form.Group>
                     <Form.Group className="mb-3 signup-label" controlId="formBasicPassword">
                         <Form.Label>Confirm Password</Form.Label>
