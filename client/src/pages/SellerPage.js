3185d306665322535bf9addd6fc29ee41e467f0a
Aaron Lu <aaronnoh0409@gmail.com>
2022-05-25 13:59:44 -0400

    added create product button to the sellers dash

--- client/src/pages/SellerPage.js
+++ client/src/pages/SellerPage.js
@@ -1,9 +1,18 @@
 import ItemList from "../components/Item";
+import { useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/esm/Button";
 
 function SellerPage() {
+    let navigate = useNavigate();
+    const redirect = () => {
+        let path = `/create`;
+        navigate(path);
+    };
+
     return (
-        <div>
+        <div className="space">
             <ItemList />
+            <Button onClick={redirect}>Create Product</Button>
         </div>
     );
 }
